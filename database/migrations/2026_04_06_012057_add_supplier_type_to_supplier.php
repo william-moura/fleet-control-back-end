1f2d07bfac649268c431de6d3e7ea570b2928218
WilliamMoura <williammoura908@gmail.com>
2026-04-05 23:08:59 -0300

    migrations de manutenç     oã

---
+++ database/migrations/2026_04_06_012057_add_supplier_type_to_supplier.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('supplier', function (Blueprint $table) {
+            $table->tinyInteger('supplier_type')->nullable()->after('supplier_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('supplier', function (Blueprint $table) {
+            $table->dropColumn('supplier_type');
+        });
+    }
+};
