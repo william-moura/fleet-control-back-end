78fda63498d6a72ad609632d7adfc1ce53bcdff3
WilliamMoura <williammoura908@gmail.com>
2026-05-18 21:22:29 -0300

    alterando horario da cron

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:notification-vehicle-fine-expire')->dailyAt('08:00');
+Schedule::command('app:notification-vehicle-fine-expire')->dailyAt('10:00');
