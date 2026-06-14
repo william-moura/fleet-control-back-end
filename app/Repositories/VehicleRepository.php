d64a97fb1afb2b77134ae9c40f5aa086a4760ca4
WilliamMoura <williammoura908@gmail.com>
2026-06-13 23:39:55 -0300

    salvar dados extra

--- app/Repositories/VehicleRepository.php
+++ app/Repositories/VehicleRepository.php
@@ -31,6 +31,11 @@ class VehicleRepository implements VehicleRepositoryInterface
             'vehicle_status' => $dto->vehicleStatus,
             'vehicle_purchase_date' => $dto->vehiclePurchaseDate,
             'vehicle_notes' => $dto->vehicleNotes,
+            'vehicle_chassis_number' => $dto->vehicleChassisNumber,
+            'vehicle_renavam_number' => $dto->vehicleRenavamNumber,
+            'vehicle_color' => $dto->vehicleColor,
+            'vehicle_transmission_type' => $dto->vehicleTransmissionType,
+            'vehicle_model_year' => $dto->vehicleModelYear,
         ]);
 
     }
