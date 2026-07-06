ceb9a77eebc38c817ac2c1891906070250e52439
Will_Moura <william@febrafar.com.br>
2026-07-06 14:19:31 -0300

    ajuste update

---
+++ app/Http/Requests/UpdateDriverRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateDriverRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'driverName' => ['required', 'string', 'max:255'],
+            'driverRegisteredNumber' => ['nullable', 'string', 'max:255'],
+            'driverAddress' => ['required', 'string', 'max:255'],
+            'driverCity' => ['required', 'string', 'max:255'],
+            'driverState' => ['required', 'string', 'max:2'],
+            'driverZipCode' => ['required', 'string', 'max:99999999'],
+            'driverBloodType' => ['required', 'string', 'max:4'],
+            'driverRg' => ['required', 'string', 'max:11'],
+            'driverCpf' => ['required', 'string', 'max:11', 'exists:drivers,driver_cpf'],
+            'driverLicenseNumber' => ['required', 'string', 'max:255'],
+            'driverLicenseExpirationDate' => ['required', 'date'],
+            'driverLicenseCategory' => ['required', 'string', 'max:3'],
+            'driverBirthDate' => ['required', 'date'],
+            'driverPhone' => ['required', 'string', 'max:15'],
+            'driverStatus' => ['required', 'integer', 'in:0,1'],
+            'photosIds' => ['nullable', 'array'],
+            'photosIds.*' => ['nullable', 'integer', 'exists:media,id'],
+            'driverNeighborhood' => ['nullable', 'string', 'max:255'],
+            'driverEmail' => ['nullable', 'email', 'max:255', 'unique:drivers,driver_email'],
+        ];
+    }
+    public function failedValidation(Validator $validator) 
+    { 
+        throw new HttpResponseException(response()->json([ 
+            'success' => false, 
+            'message' => 'Erros de validaçãos', 
+            'data' => $validator->errors() 
+        ], 422)); 
+    }
+
+}
